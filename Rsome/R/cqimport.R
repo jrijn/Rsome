5519855299f4287ec326ca95ded88bf30d2c91b5
Jorik van Rijn <jorik.vanrijn@gmail.com>
2019-06-28 11:59:51 +0200

    Add files via upload

---
+++ Rsome/R/cqimport.R
@@ -0,0 +1,72 @@
+#' Import BioRad qPCR Data
+#'
+#' This function imports a .txt file from CFX manager and returns a dataframe.
+#' 
+#' First simply use the file name (without .txt) as input, assuming subfolder /input as source.
+#' The Cq is set to 40 in those wells were the threshold was not reached.
+#' 
+#' @param tablename: This is the filename of the .txt file containing Cq values as copied from the file manager. 
+#' @param dropunnecessary: Defaults to TURE. If TRUE, returns only the columns "Well", "Target", "Sample", "Cq".
+#' @keywords qPCR, import
+#' @export cqimport
+#' @examples
+#' #The .txt files should be in: "parent_directory/input/example_file_name.txt"
+#' 
+#' setwd("parent_directory")
+#' filename <- "example_file_name"
+#' df <- cqimport(filename)
+
+cqimport <- function(tablename, dropunnecessary = TRUE){
+  
+  #First compile the .txt file name, assuming subfolder /input as source.
+  #Then import the table with headings, assuming decimal separator ",".
+ 
+  filename <- paste("input/", tablename, ".txt", sep = "")
+  
+  df <- read.table(
+    filename, 
+    header=TRUE, 
+    na.strings=c("", "NA"), 
+    sep = "\t", 
+    check.names = TRUE, 
+    dec = ","
+  )
+  
+  #If the column "Cq" does not contain numeric values, import again with decimal separator "."
+  
+  cqclass <- sapply(df, class)
+  
+  if(cqclass["Cq"] != "numeric") {
+    
+    df <- read.table(
+      filename, 
+      header=TRUE, 
+      na.strings=c("", "NA"), 
+      sep = "\t", 
+      check.names = TRUE, 
+      dec = "."
+    )
+    
+  }
+  
+  #Now clean up the unnecessary columns and drop empty wells.
+  
+  if(dropunnecessary == TRUE){
+    
+    import::from(dplyr, select, filter)
+    import::from(magrittr, "%>%")
+    
+    df <- df %>%
+      select(Well, Target, Sample, Cq) %>%
+      filter(Target != "NaN")
+    
+  }
+  
+  #Finally, set the Cq to 40 in those wells were the threshold was not reached.
+  
+  df$Cq[is.nan(df$Cq)] <- 40
+  
+  #Return the resulting dataframe!
+  
+  return(df)
+}
---
+++ Rsome/R/replot.R
@@ -0,0 +1,38 @@
+#' Plot Relative Expression Values
+#'
+#' This function plots the relative expression values for each target normalized to the household gene. 
+#' 
+#' @param tablename: a relex object
+#' @param groupColumn: defaults to "Sample". Defines the grouping column which will define coloring.
+#' @keywords qPCR, relative expression, plot
+#' @export re.plot
+#' @examples
+#' 
+#' df <- cqimport(filename)
+#' re <- relex(df, household = "Gapdh")
+#' p1 <- re.plot(re)
+#' 
+
+re.plot <- function(tablename, groupColumn = "Sample"){
+  
+  library(ggplot2)
+  import::from(dplyr, sym)
+  
+  p1 <- ggplot(tablename, aes(x=Sample, y=rel.expression), group = !!sym(groupColumn))+
+    scale_y_continuous(limits=c(0,NA))+
+    geom_bar(
+      aes(fill=!!sym(groupColumn)), 
+      stat="identity", 
+      position="dodge")+
+    facet_wrap(
+      ~ Target, 
+      scales = "free")+
+    theme_classic()+
+    theme(
+      axis.text.x = element_text(angle = 30, hjust = 1))+
+    geom_hline(
+      yintercept=0,
+      size=.5)
+  
+  return(p1)
+}
