c403f4b7354c04c1a47cd9a5ed73d5b89f5f33e4
Jorik van Rijn <jorik.vanrijn@gmail.com>
2020-04-07 15:54:59 +0200

    Update cqplot.R

--- R/cqplot.R
+++ R/cqplot.R
@@ -28,10 +28,9 @@ cq.plot <- function(tablename, groupColumn = "Sample", plotWidth = 10, plotHeigh
   
   #Then plot the Cq values, faceting is set to the target gene.
   
-  p1 <- ggplot(samples, aes(x=Sample, y=Cq), group = Sample)+
+  p1 <- ggplot(samples, aes(x=Sample, y=Cq))+
     scale_y_continuous(limits=c(12.5,NA))+
     geom_dotplot(
-      aes(fill=factor(Sample)), 
       binaxis = "y",
       stackdir = "center")+
     facet_wrap(
